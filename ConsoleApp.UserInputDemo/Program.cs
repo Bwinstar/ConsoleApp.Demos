d739252a907b7a8b4b1d61a75d9851cc6371ba48
Bwinstar <kwinnerezinne@gmail.com>
2024-03-22 20:43:13 +0100

    Added firstname and lastname prompt

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,14 +1,19 @@
 ﻿// This Project Is used to collect the name and age of Users and Determine their working Age Remaining from the retirement age
 
 //Declaring Variables
-string name = string.Empty;
+using System.Xml.Linq;
+
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
 int retirementAge = 70;
 
 /// Prompting User for Input
 
-Console.WriteLine("Please enter your name");
-name = Console.ReadLine();
+Console.WriteLine("Please enter your First name");
+firstName = Console.ReadLine();
+Console.WriteLine("Please enter your Last name");
+lastName = Console.ReadLine();
 Console.WriteLine(" Please enter your Age");
 age = Convert.ToInt32(Console.ReadLine());
 
@@ -16,6 +21,6 @@ age = Convert.ToInt32(Console.ReadLine());
 int workingYearsRemaining = retirementAge - age;
 
 // Output the result to the Users
-Console.WriteLine($"Full Name :  {name}");
+Console.WriteLine($"Full Name :  {firstName} {lastName}");
 Console.WriteLine($" Age : {age}");
 Console.WriteLine($" Working Years Remaining : {workingYearsRemaining}");
